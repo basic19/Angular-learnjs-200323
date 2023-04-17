7a1a36815d15e820ce81908eb75d76aaf974415d
Василий Кузнецов <basic-1@mail.ru>
2023-04-17 03:20:03 +0300

    feat: master

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,12 @@
-import {Component, ViewEncapsulation} from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {applicationConfigMock} from './shared/application-config/application-config.mock';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.css'],
-	encapsulation: ViewEncapsulation.Emulated,
+	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-	title = 'Angular-learnjs-200323';
-
-	onClick() {
-		console.log('Click AppComponent');
-	}
-
-	onKeyDownEnter(event: any) {
-		console.log(event);
-
-		console.log('onKeyDown - Enter');
-	}
+	readonly applicationConfig = applicationConfigMock;
 }
